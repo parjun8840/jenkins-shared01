614b1c435e8d814c975f94b850dd357b7c41adc1
parjun8840 <54829402+parjun8840@users.noreply.github.com>
2021-03-28 18:55:24 +0530

    Updated

--- vars/basicshared.groovy
+++ vars/basicshared.groovy
@@ -1,3 +1,37 @@
 def printMyName(name){
 	echo "${name}"
+	def multiSelect= new ExtendedChoiceParameterDefinition("name", 
+            "PT_MULTI_SELECT", 
+            "blue,green,yellow,blue", 
+            "project name",
+            "", 
+            "",
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "blue,green,yellow,blue", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            false,
+            false, 
+            3, 
+            "multiselect", 
+            ",") 
+
+   def userInput = input  id: 'customID', message: 'Let\'s promote?', ok: 'Release!', parameters:  [multiSelect]
+
 }
