a1fdf7fb289ddf7bfd9c4cb860b565663c8e6d01
parjun8840 <54829402+parjun8840@users.noreply.github.com>
2021-03-28 18:24:39 +0530

    vars

---
+++ vars/BasicShared.groovy
@@ -0,0 +1,36 @@
+
+package com.basicsShared;
+import com.cwctravel.hudson.plugins.extended_choice_parameter.ExtendedChoiceParameterDefinition
+def multiSelect= new ExtendedChoiceParameterDefinition("name", 
+            "PT_MULTI_SELECT", 
+            "blue,green,yellow,blue", 
+            "project name",
+            "", 
+            "",
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "blue,green,yellow,blue", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            false,
+            false, 
+            3, 
+            "multiselect", 
+            ",") 
+
+   def userInput = input  id: 'customID', message: 'Let\'s promote?', ok: 'Release!', parameters:  [multiSelect]
