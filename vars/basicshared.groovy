130160d4d025d6f7bfba5011dbd2ea42961f8cd4
parjun8840 <54829402+parjun8840@users.noreply.github.com>
2021-04-08 15:31:54 +0530

    Update basicshared.groovy

--- vars/basicshared.groovy
+++ vars/basicshared.groovy
@@ -32,10 +32,12 @@ def printMyName(name){
             3, 
             "multiselect", 
             ",") 
+	
+echo "The value of Global variable: ${global_varirable}"
 
    def userInput = input  id: 'customID', message: 'Let\'s promote?', ok: 'Release!', parameters:  [multiSelect]
 	return 'My name is ' + "${name}"
 	
-	echo "The value of Global variable: ${global_varirable}"
+	
 
 }
